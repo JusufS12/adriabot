b7ebdd472726d9005dec8f8641126e8c4222ec18
Josip Sikiric <josip.sikiric@proton.me>
2024-03-05 12:41:54 +0100

    initial commit

---
+++ valjdaCeRaditi.ino
@@ -0,0 +1,89 @@
+#include <AFMotor.h>
+#include <HCSR04.h>
+
+#define SS 25
+#define LS 47
+#define DS 46
+
+#define SPEED 250
+
+void lineFollower();
+
+AF_DCMotor dM4(4);
+AF_DCMotor lM3(3);
+
+HCSR04 zvucni(22, 23);
+
+void setup() {
+  // turn on motor
+  // Serial.begin(9600);
+  lM3.setSpeed(SPEED);
+  dM4.setSpeed(SPEED - 50);
+  lM3.run(RELEASE);
+  dM4.run(RELEASE);
+}
+
+void loop() {
+  // ponavlja nekom frekvencijom (DELAY / 1000; vjerojatno)
+  // if (zvucni.dist() > 6 || !zvucni.dist()) {
+  //   lineFollower();
+  // }
+  // else {
+  //   lM3.run(RELEASE);
+  //   dM4.run(RELEASE);
+  // }
+  lineFollower();
+}
+
+
+void lineFollower() {
+  if (digitalRead(SS)) {
+    if (!digitalRead(LS) && !digitalRead(DS)) {
+      // vozi ravno
+      lM3.run(FORWARD);
+      dM4.run(FORWARD);
+    }
+
+    if (digitalRead(LS) && !digitalRead(DS)) {
+      // ostro livo
+      lM3.run(BACKWARD);
+      dM4.run(FORWARD);
+    }
+
+    if (!digitalRead(LS) && digitalRead(DS)) {
+      // ostro desno
+      lM3.run(FORWARD);
+      dM4.run(BACKWARD);
+    }
+
+    if (digitalRead(LS) && digitalRead(DS)) {
+      // stani
+      lM3.run(RELEASE);
+      dM4.run(RELEASE);
+    }
+  } else {
+    if (digitalRead(LS) && !digitalRead(DS)) {
+      // livo
+      lM3.run(RELEASE);
+      dM4.run(FORWARD);
+    }
+
+    if (!digitalRead(LS) && digitalRead(DS)) {
+      // desno
+      lM3.run(FORWARD);
+      dM4.run(RELEASE);
+    }
+
+    if (!digitalRead(LS) && !digitalRead(DS)) {
+      // nazad
+      lM3.run(BACKWARD);
+      dM4.run(BACKWARD);
+    }
+
+    // if (digitalRead(LS) && digitalRead(DS)) {
+    //   // naprid
+    //   lM3.run(FOWARD);
+    //   dM4.run(FOWARD);
+    // }
+  }
+}
